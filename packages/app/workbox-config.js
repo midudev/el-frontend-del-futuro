89972fd0baafe1507fe1b18b05ada22d93020e2b
midudev <miduga@gmail.com>
2019-04-25 23:51:00 +0200

    Update sw

--- packages/app/workbox-config.js
+++ packages/app/workbox-config.js
@@ -3,5 +3,21 @@ module.exports = {
   'globPatterns': [
     '**/*.{js,html,txt,webp,gif,jpg,webm,png,ico,svg,json}'
   ],
-  'swDest': 'src/workers/sw.js'
+  'swDest': 'src/workers/sw.js',
+
+  runtimeCaching: [{
+    urlPattern: /\.(?:js,html,txt,webp,gif,jpg,webm,png,ico,svg,json)$/,
+
+    // Apply a cache-first strategy.
+    handler: 'StaleWhileRevalidate',
+
+    options: {
+      // Use a custom cache name.
+      cacheName: 'statics',
+
+      expiration: {
+        maxEntries: 100
+      }
+    }
+  }]
 }
