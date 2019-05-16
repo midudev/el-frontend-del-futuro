5d861aa16811d5bd4c81d6aace7eb20517f7c619
midudev <miduga@gmail.com>
2019-05-16 12:56:39 +0200

    Add image and recognize voice

--- packages/app/src/main/app.js
+++ packages/app/src/main/app.js
@@ -70,13 +70,6 @@ function initRouter () {
 
 initRouter()
 
-const voiceMessages = [
-  'No te he entendido bien.',
-  'Ja ja ja. Te había entendido antes. Tienes 11 noticias por leer.',
-  'Gracias las que tu tienes. Un saludo a la ExFest!'
-]
-let i = 0
-
 const synth = window.speechSynthesis
 
 document.addEventListener('keydown', function (e) {
@@ -84,10 +77,45 @@ document.addEventListener('keydown', function (e) {
     const recognition = new webkitSpeechRecognition()
 
     recognition.lang = 'es-ES'
-    recognition.onresult = function() {
-      const voice = new SpeechSynthesisUtterance(voiceMessages[i])
-      synth.speak(voice)
-      i++
+    recognition.onresult = function(e) {
+      const message = e.results[0][0].transcript
+      let articles
+      try {
+        articles = Array.from(document.querySelector("x-pages-home").shadowRoot.querySelector("x-news").shadowRoot.querySelectorAll("x-article")).slice(0, 2)
+      } catch (e) {}
+
+      if (message === 'léeme las noticias') {
+        const voice = new SpeechSynthesisUtterance('¡Vale Miguel Ángel! Las tres primeras son...')
+        synth.speak(voice)
+        articles.forEach(el => {
+          const title = el.getAttribute('title')
+          const voice = new SpeechSynthesisUtterance(title)
+          synth.speak(voice)
+        })
+        const againVoice = new SpeechSynthesisUtterance('¿Te puedo ayudar en algo más?')
+        synth.speak(againVoice)
+        return
+      }
+
+      if (message.includes('entra en la')) {
+        const positions = ['primera', 'segunda', 'tercera']
+        const index = positions.findIndex(position => message.includes(position)) || 0
+        console.log(index)
+        const voice = new SpeechSynthesisUtterance(`Entrando en la ${positions[index]}`)
+        synth.speak(voice)
+        const href = articles[index].shadowRoot.querySelector('x-anchor').getAttribute('href')
+        window.history.pushState({}, '', href)
+        events.emit('navigation')
+        const againVoice = new SpeechSynthesisUtterance('¿Te puedo ayudar en algo más?')
+        synth.speak(againVoice)
+        return
+      }
+
+      if (message.includes('gracias')) {
+        const voice = new SpeechSynthesisUtterance('¡Gracias a ti! Un saludo a todos los asistentes del meetup. ¡Moláis mucho!')
+        synth.speak(voice)
+        return
+      }
     }
     recognition.start()
   }
