b7a96d4c47584ab99dc00444fa04bfb768b80060
midudev <miduga@gmail.com>
2019-11-09 00:00:39 +0100

    Rewrite router to be more approachable

---
+++ packages/app/src/main/router.js
@@ -0,0 +1,72 @@
+import events from './events.js'
+
+const ROUTES = [
+  {
+    path: '/',
+    component: 'x-pages-home',
+    module: '/pages/home.js'
+  },
+  {
+    path: '/category/:id',
+    component: 'x-pages-home',
+    module: '/pages/home.js'
+  },
+  {
+    path: '/article/:id',
+    component: 'x-pages-article',
+    module: '/pages/article.js'
+  },
+  {
+    path: '/favs',
+    component: 'x-pages-favs',
+    module: '/pages/favs.js'
+  },
+  {
+    path: '/videos',
+    component: 'x-pages-videos',
+    module: '/pages/videos.js'
+  }
+]
+
+const matchRoute = route => {
+  const { pathname } = window.location
+  const { path } = route
+  const dynamicPath = path.replace(/:[^\s/]+/g, '([\\w-]+)')
+  const matcher = new RegExp(`^${dynamicPath}$`)
+  return pathname.match(matcher)
+}
+
+const createCheckPathAgainstRoutes = ({ routerEl }) => () => {
+  const {
+    component,
+    module: moduleToImport,
+    ...matchedRoute
+  } = ROUTES.find(matchRoute)
+  // extract the id. It's on our case always the second segment
+  const [, id] = matchRoute(matchedRoute)
+
+  // if we component is already loaded and we have an id
+  // we only have to change the attribute
+  // to avoid white blink
+  const { firstElementChild: actualRouteComponent } = routerEl
+  if (actualRouteComponent?.tagName?.toLowerCase() === component) {
+    return id && actualRouteComponent.setAttribute('id', id)
+  }
+
+  // dynamically import the module and then render it
+  import(moduleToImport).then(() => {
+    const idAttr = id ? `id=${id}` : ''
+    routerEl.innerHTML = `<${component} ${idAttr}></${component}>`
+  })
+}
+
+export const initializeRouter = ({ routerId = 'router' } = {}) => {
+  const routerEl = document.getElementById(routerId)
+  const checkPathAgainstRoutes = createCheckPathAgainstRoutes({ routerEl })
+  // check path for the first time
+  checkPathAgainstRoutes()
+  // and then check the path everytime we do a navigation
+  events.on('navigation', checkPathAgainstRoutes)
+  // and when the onpopstate fires
+  window.onpopstate = checkPathAgainstRoutes
+}
