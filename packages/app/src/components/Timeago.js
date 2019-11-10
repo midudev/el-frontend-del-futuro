ea9b6d672fb6423204d3eb95775285a0ff5ca355
midudev <miduga@gmail.com>
2019-11-10 02:09:22 +0100

    Simplify timeago

--- packages/app/src/components/Timeago.js
+++ packages/app/src/components/Timeago.js
@@ -2,30 +2,32 @@ import { Component } from '../main/Component.js'
 
 export const Timeago = 'x-timeago'
 
-const diffFromNow = ts => Math.floor((ts - +Date.now()) / 1000 / 60 / 60)
+const getDaysDiffFromNow = ts =>
+  Math.floor((ts - +Date.now()) / 1000 / 60 / 60)
+
 /*
-  const rtf = new Intl.RelativeTimeFormat('es', { style: 'short' })
-  const diff = diffFromNow(articleDate)
-  const timeago = diff > -24
+  const rtf = new Intl.RelativeTimeFormat('es-ES')
+  const diff = getDaysDiffFromNow(timestamp)
+  date = diff > -24 // la diferencia es de menos de un dia
     ? rtf.format(diff, 'hours')
     : rtf.format(Math.floor(diff / 24), 'days')
 */
 window.customElements.define(Timeago, class extends Component {
   styles () {
-    return 'time { color: #555; font-size: 14px; letter-spacing: 1px; }'
+    return `
+    time {
+      color: var(--text);
+      opacity: .8;
+      font-size: 14px;
+      letter-spacing: 1px;
+    }`
   }
 
   render ({ attrs }) {
     if (!attrs.date || attrs.date === 'undefined') return ''
     const timestamp = +attrs.date
-    // const timeago = new Date(timestamp)
+    let date = new Date(timestamp)
 
-    const rtf = new window.Intl.RelativeTimeFormat('es', { style: 'short' })
-    const diff = diffFromNow(timestamp)
-    const timeago = diff > -24
-      ? rtf.format(diff, 'hours')
-      : rtf.format(Math.floor(diff / 24), 'days')
-
-    return `<time>${timeago}</time>`
+    return `<time>${date}</time>`
   }
 })
